bee1746726ee8daeea2e97074517c03d6c97c573
Duy Ngoc Nguyen <dnguyen@opentext.com>
2018-12-17 18:39:01 -0800

    callback for submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
 
 export default class App extends React.Component {
+    
+    onTermSubmit = (term) => {
+        console.log(term);
+        // youtube.get('/search')
+    };
+
     render() {
         return (
             <div className="ui container">
-                <SearchBar />
+                <SearchBar onFormSubmit={this.onTermSubmit} />
             </div>
         );
     }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,22 +1,27 @@
 import React from 'react';
 
 export default class SearchBar extends React.Component {
-    state = { term: ''};
+    constructor(props){
+        super(props);
+        this.state = { term: ''};
+        this.onTermSubmit = this.onTermSubmit.bind(this);        
+    }
 
-    onInputChange = e => {
+    onInputChange = (e) => {
         this.setState({
             term: e.target.value
         });
     };
 
-    onFormSubmit = e => {
+    onTermSubmit = (e) => {
         e.preventDefault();
-    }
+        this.props.onFormSubmit(this.state.term);
+    };
 
     render () {
         return (
             <div className="search-bar ui segment">
-                <form onSubmit={this.onFormSubmit} className="ui form">
+                <form onSubmit={this.onTermSubmit} className="ui form">
                     <div className="field">
                         <label>Video Search</label>
                         <input 
