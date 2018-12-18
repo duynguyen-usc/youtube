8baa48fce80eba1a893cf5004aa5e3e4e58d02f7
Duy Ngoc Nguyen <dnguyen@opentext.com>
2018-12-17 17:34:02 -0800

    add search bar component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class SearchBar extends React.Component {
+    render () {
+        return (
+            <div className="search-bar ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label>Video Search</label>
+                        <input />
+                    </div>
+                </form>
+                
+            </div>
+        );
+    }
+}
