d9aec2a64b23e13a7e5c54b4dfcc43eea89dee79
Duy <duy@duys-mbp.home>
2018-12-18 18:21:28 -0800

    embed video

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -5,8 +5,11 @@ const VideoDetail = ({selectedVideo}) => {
         return <div>Loading</div>;
     }
 
+    const videoSrc = `http://www.youtube.com/embed/${selectedVideo.id.videoId}`
+
     return (
         <div className="ui segment">
+            <iframe src={videoSrc}/>
             <h4 className="ui header">
                 {selectedVideo.snippet.title}
             </h4>
