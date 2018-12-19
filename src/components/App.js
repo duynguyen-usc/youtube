364059ae035910cd68bf755ba868b92906008f8f
Duy <duy@duys-mbp.home>
2018-12-18 18:35:33 -0800

    add to grid

--- src/components/App.js
+++ src/components/App.js
@@ -30,8 +30,16 @@ export default class App extends React.Component {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
-                <VideoDetail selectedVideo={this.state.selectedVideo} />
-                <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail selectedVideo={this.state.selectedVideo} />
+                        </div>
+                        <div className="five wide column">
+                            <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
